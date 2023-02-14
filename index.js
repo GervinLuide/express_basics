6724d4f77d10ae3f1d308495663f476f2a3da62b
Gervin Luide <gervin.luide@khk.ee>
2023-02-14 12:43:01 +0200

    templating directory setup

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const path = require("path");
 const app = express();
 // add public directory
 app.use(express.static('public'));
+//ass views directory path
+app.set('views', path.join(__dirname, 'views'));
+// add views template engine
+app.set('view engine', 'ejs');
 
 app.get('/user/:username',(req, res)=>{
     let user = req.params.username;
-    res.render('index.ejs', {username : user});
+    res.render('index', {username : user});
 });
 
 app.listen(3000,()=>{
